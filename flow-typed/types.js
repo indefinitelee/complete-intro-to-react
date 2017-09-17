2dd1c80cbbe6d3b086a28b528ac2ada3be9921de
lee <leerwilson@gmail.com>
2017-09-17 16:57:36 -0400

    still need types, details component and route

---
+++ flow-typed/types.js
@@ -0,0 +1,14 @@
+export type show = {
+  title: string,
+  description: string,
+  year: string,
+  imdbID: string,
+  trailer: string,
+  poster: string
+};
+
+// declare var module: {
+//   hot: {
+//     accept(path: string, callback: () => void): void
+//   }
+// };
