6818e5327694377c54fc611da4b73f2dd3e052d6
lee <leerwilson@gmail.com>
2017-09-17 23:14:57 -0400

    combine reducers, fix flow types, working

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,6 +1,8 @@
+// @flow
+
 import { SET_SEARCH_TERM } from './actions';
 
 // eslint-disable-next-line import/prefer-default-export
-export function setSearchTerm(searchTerm) {
+export function setSearchTerm(searchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
 }
