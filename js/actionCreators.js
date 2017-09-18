b828e3829bb42d2d861dee8b4fe4d2307ed3d0a0
lee <leerwilson@gmail.com>
2017-09-17 22:27:48 -0400

    map state to props, dispatch actions

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,5 +1,5 @@
 import { SET_SEARCH_TERM } from './actions';
 
-export function serSearchTerm(searchTerm) {
+export default function serSearchTerm(searchTerm) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
 }
